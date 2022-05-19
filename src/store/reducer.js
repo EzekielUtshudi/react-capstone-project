13a5ebe22c8dd38127773cf26481ba5006be3bde
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-05-19 09:24:41 +0200

    deployment

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,13 @@
 const GET_STATS = 'covid/GET_STATS';
 
-const intialState = [];
+const initialState = [];
 
 const GetStats = (payload) => ({
   type: GET_STATS,
   payload,
 });
 
-const StatsReducer = (state = intialState, action) => {
+const StatsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_STATS:
       return [...state, ...action.payload];
